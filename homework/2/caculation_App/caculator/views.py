ec6d407b8d90c897fcb76223523ebfce807a0767
nicholeemma <790956043@qq.com>
2019-09-08 13:44:30 -0400

    Changed to GET method, updated view.py

--- homework/2/caculation_App/caculator/views.py
+++ homework/2/caculation_App/caculator/views.py
@@ -4,36 +4,49 @@ first_number=null
 second_number=null
 character=null
 
-def update(i):
-    if i.isdigit() and first_number==null and character==null:
-        first_number=i
-    if i.isdigit() and first_number!=null and character==null:
-        first_number=first_number*10+i
-    if character!=null and second_number==null:
-        second_number=i
+
     
 # Create your views here.
 def caculate(request):
     content = {}
     content['show'] = '0'
-	content['first_operator'] = ''
-    content['second_operator'] = ''
-	content['current_status'] = 'idle'
-	content['first_number'] = '0'
-    content['second_number'] = null
+    # content['first_operator'] = ''
+    # content['second_operator'] = ''
+	# content['current_status'] = 'idle'
+	# content['first_number'] = '0'
+    # content['second_number'] = null
+    if (isValid(request)):
+
     
-    if request.method == "POST":
-        if "button1" in request.POST:
-            if content['first_number'] == '0':
-                content['first_number']=1
-            if content['first_number'] != '0' and content['operator'] == '':
-                content['first_number']=str(10*content['first_number']+1)
-            if content['operator'] != '' and content['second_number'] == null:
-                content['second_number']=1
-            if content['operator'] != '' and content['second_number'] != null:
-                content['second_number']=str(10*content['second_number']+1)
+# The GET information is a dictionary
+        if "number" in request.GET:
+            if content['show'] == request.GET.number:
+            #     content['first_number']=1
+            # if content['first_number'] != '0' and content['operator'] == '':
+            #     content['first_number']=str(10*content['first_number']+1)
+            # if content['operator'] != '' and content['second_number'] == null:
+            #     content['second_number']=1
+            # if content['operator'] != '' and content['second_number'] != null:
+            #     content['second_number']=str(10*content['second_number']+1)
 
     
     return render(request, 'index.html', content)
+
+# According the eighth requirement in the Additional Requirement, 
+# users may send randome request, 
+# that is why a validation needs to be implemented
+def isValid(request):
+    number_list = ['1','2','3','4','5','6','7','8','9','0']
+    operator_list = ['+','-','*','÷']
+    if ((not 'number' in request.GET) and (not 'operator' in request.GET)):
+		return False
+    if 'number' in request.GET and not request.GET.get('number') in number_list:
+		return False
+	if 'operator' in request.GET:
+		if not request.GET.get('operator').strip('\u200b') in operator_list:
+			return False
+
+
+
         
 
