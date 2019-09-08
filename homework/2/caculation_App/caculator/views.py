b62f50e245f39a38455c44b8a062bf73549a5347
nicholeemma <790956043@qq.com>
2019-09-08 14:22:58 -0400

    Changed the path of templates

--- homework/2/caculation_App/caculator/views.py
+++ homework/2/caculation_App/caculator/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-first_number=null
-second_number=null
-character=null
+# first_number=null
+# second_number=null
+# character=null
 
 
     
@@ -16,11 +16,12 @@ def caculate(request):
 	# content['first_number'] = '0'
     # content['second_number'] = null
     if (isValid(request)):
+        print('valid')
 
     
 # The GET information is a dictionary
         if "number" in request.GET:
-            if content['show'] == request.GET.number:
+            content['show'] == request.GET.number
             #     content['first_number']=1
             # if content['first_number'] != '0' and content['operator'] == '':
             #     content['first_number']=str(10*content['first_number']+1)
@@ -29,8 +30,7 @@ def caculate(request):
             # if content['operator'] != '' and content['second_number'] != null:
             #     content['second_number']=str(10*content['second_number']+1)
 
-    
-    return render(request, 'index.html', content)
+    return render(request, "index.html", content)
 
 # According the eighth requirement in the Additional Requirement, 
 # users may send randome request, 
@@ -39,12 +39,12 @@ def isValid(request):
     number_list = ['1','2','3','4','5','6','7','8','9','0']
     operator_list = ['+','-','*','÷']
     if ((not 'number' in request.GET) and (not 'operator' in request.GET)):
-		return False
+        return False
     if 'number' in request.GET and not request.GET.get('number') in number_list:
-		return False
-	if 'operator' in request.GET:
-		if not request.GET.get('operator').strip('\u200b') in operator_list:
-			return False
+        return False
+    if 'operator' in request.GET:
+        if not request.GET.get('operator').strip('\u200b') in operator_list:
+            return False
 
 
 
