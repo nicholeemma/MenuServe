d2cb2bdd9e40d810688501ad5b71861e02ce22c7
nicholeemma <790956043@qq.com>
2019-09-08 21:07:52 -0400

    Filled in logic in view.py

--- homework/2/caculation_App/caculator/views.py
+++ homework/2/caculation_App/caculator/views.py
@@ -5,40 +5,99 @@ import datetime
 # second_number=null
 # character=null
 
-
+content = {}
+content['first_operator'] = ''
+content['second_operator'] = ''
+content['error'] = ''
+content['first_number'] = '0'
+content['second_number'] = ''
     
 # Create your views here.
 def caculate(request):
-
-
+    '''
+    content: a dictionary of information post back to the client side
+    ['first_operator'] stores the number
+    ['second_operator']
+    ['first_number']
+    ['second_number']
+    '''
     content = {}
-    content['show'] = '0'
-    if "number" in request.POST:
-        content['show'] = request.POST.get('number')
-        #request.GET.get('textbox') = request.GET.get('number')
-    # content['first_operator'] = ''
-    # content['second_operator'] = ''
-	# content['current_status'] = 'idle'
-	# content['first_number'] = '0'
-    # content['second_number'] = null
-    if (isValid(request)):
-        
-        print('valid')
-
+    content['first_operator'] = ''
+    content['second_operator'] = ''
+    content['error'] = ''
+    content['first_number'] = '0'
+    content['second_number'] = ''
     
-# The GET information is a dictionary
-        # if "number" in request.GET:
-        #     content['show'] == request.GET.number
-        #     request.GET.textbox = request.GET.number
-            #     content['first_number']=1
-            # if content['first_number'] != '0' and content['operator'] == '':
-            #     content['first_number']=str(10*content['first_number']+1)
-            # if content['operator'] != '' and content['second_number'] == null:
-            #     content['second_number']=1
-            # if content['operator'] != '' and content['second_number'] != null:
-            #     content['second_number']=str(10*content['second_number']+1)
-            # print(context['current_status'])
+    content['show'] = '0'
+    if True:
+        if "number" in request.POST:
+            if request.POST.get('first_number')=='0' and request.POST.get('second_number') =='' and request.POST.get('first_operator') == '' and request.POST.get('second_operator') == '':
+                content['first_number'] = request.POST.get('number')
+                content['show'] = request.POST.get('number')
+            elif request.POST.get('first_number')!='0' and request.POST.get('second_number') =='' and request.POST.get('first_operator') == '' and request.POST.get('second_operator') == '':
+                content['first_number'] = str(request.POST.get('first_number'))+str(request.POST.get('number'))
+                content['show'] = str(request.POST.get('first_number'))+str(request.POST.get('number'))
+            elif request.POST.get('first_number')!='0' and request.POST.get('second_number') =='' and request.POST.get('first_operator') != '' and request.POST.get('second_operator') == '':
+            
+                content['second_number'] = request.POST.get('number')
+                content['show'] = request.POST.get('number')
+            elif request.POST.get('first_number')!='0' and request.POST.get('second_number') !='' and request.POST.get('first_operator') != '' and request.POST.get('second_operator') == '':
+                content['second_number'] = str(request.POST.get('second_number'))+str(request.POST.get('number'))
+                
+                content['show'] = str(request.POST.get('second_number'))+str(request.POST.get('number'))
+            
+        elif "operator" in request.POST:
+            if request.POST.get('first_number')=='0' and request.POST.get('second_number') =='' and request.POST.get('first_operator') == '' and request.POST.get('second_operator') == '':
+                content['first_operator'] = request.POST.get('operator')
+                content['show']=request.POST.get('first_number')
+            elif request.POST.get('first_number')!='0' and request.POST.get('second_number') =='' and request.POST.get('first_operator') == '' and request.POST.get('second_operator') == '':
+            
+                content['first_operator'] = request.POST.get('operator')
+                content['show']=request.POST.get('first_number')
+            elif request.POST.get('first_number')!='0' and request.POST.get('second_number') =='' and request.POST.get('first_operator') != '' and request.POST.get('second_operator') == '':
+            
+                content['first_operator'] = request.POST.get('operator')
+                content['show']=request.POST.get('first_number')
+            elif request.POST.get('first_number')!='0' and request.POST.get('second_number') !='' and request.POST.get('first_operator') != '' and request.POST.get('second_operator') == '':
+            
+                content['second_operator'] = request.POST.get('operator')
+                if request.POST.get('first_operator')!='=':
+                    if request.POST.get('first_operator').strip('\u200b') =='+': 
+                        content['show']=request.POST.get('first_number')+request.POST.get('second_operator')
+                        content['first_number']=request.POST.get('first_number')+request.POST.get('second_operator')
+                    elif request.POST.get('first_operator').strip('\u200b') =='-':
+                        content['show']=request.POST.get('first_number')-request.POST.get('second_operator')
+                        content['first_number']=request.POST.get('first_number')-request.POST.get('second_operator')
+                        
+                    elif request.POST.get('first_operator').strip('\u200b') =='*':
+                        content['show']=request.POST.get('first_number')*request.POST.get('second_operator')
+                        content['first_number']=request.POST.get('first_number')*request.POST.get('second_operator')
+                        
+                    elif request.POST.get('first_operator').strip('\u200b') =='÷':
+                        content['show']=(int)(request.POST.get('first_number')/request.POST.get('second_operator'))
+                        content['first_number']=(int)(request.POST.get('first_number')/request.POST.get('second_operator'))
+                        
+                else:
+                    if request.POST.get('first_operator').strip('\u200b') =='+': 
+                        
+                        content['show']=request.POST.get('first_number')+request.POST.get('second_operator')
+                    elif request.POST.get('first_operator').strip('\u200b') =='-':
+                       
+                        content['show']=request.POST.get('first_number')-request.POST.get('second_operator')
+                    elif request.POST.get('first_operator').strip('\u200b') =='*':
+                        
+                        content['show']=request.POST.get('first_number')*request.POST.get('second_operator')
+                    elif request.POST.get('first_operator').strip('\u200b') =='÷':
+                        
+                        content['show']=(int)(request.POST.get('first_number')+request.POST.get('second_operator'))
 
+                # clear        
+                content['first_operator'] = ''
+                content['second_operator'] = ''
+                
+                content['first_number'] = '0'
+                content['second_number'] = ''
+            
     return render(request, "index.html", content)
 
 # According the eighth requirement in the Additional Requirement, 
@@ -51,7 +110,7 @@ def isValid(request):
     operator_list = ['+','-','*','÷']
     if ((not 'number' in request.POST) and (not 'operator' in request.POST)):
         return False
-    if 'number' in request.POST and not request.GET.get('number') in number_list:
+    if 'number' in request.POST and not request.POST.get('number') in number_list:
         return False
     if 'operator' in request.POST:
         if not request.POST.get('operator').strip('\u200b') in operator_list:
