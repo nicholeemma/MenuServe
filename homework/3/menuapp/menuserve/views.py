152a5c3ad3ccc2bb1a517eeb9fde619e288d6b74
nicholeemma <790956043@qq.com>
2019-09-13 18:25:31 -0400

    Updated view.py and add templates and static

--- homework/3/menuapp/menuserve/views.py
+++ homework/3/menuapp/menuserve/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Menu, Store, Employee, Manager, Order
+
+def index(request):
+    content={}
+    return render(request,content)
 
 # Create your views here.
