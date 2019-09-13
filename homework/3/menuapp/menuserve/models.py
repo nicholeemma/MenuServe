1309c9d6a9f7d297b6808f1de475050c2ad81a0a
nicholeemma <790956043@qq.com>
2019-09-13 18:11:11 -0400

    Fix the relations in the model.py

--- homework/3/menuapp/menuserve/models.py
+++ homework/3/menuapp/menuserve/models.py
@@ -1,38 +1,43 @@
+
+# The order of classes matters, the below class will be based on the above ones
+
 from django.db import models
 from django.utils import timezone
 # Code reference: https://medium.com/fbdevclagos/how-to-build-a-todo-app-with-django-17afdc4a8f8c
-# Create your models here.
+
+
 class Menu(models.Model): 
+    
     name_of_cuisine = models.CharField(max_length=30)
     id_for_dish = models.CharField(max_length=4)
     price = models.IntegerField()
     classification = models.CharField(max_length=10)
     description = models.CharField(max_length=100)
 
+class Store(models.Model):
+       
+    location = models.CharField(max_length=100)
+    name = models.CharField(max_length=10)
+
+class Manager(models.Model):
+
+    name = models.CharField(max_length=20)
+    m_store = models.ForeignKey(Store,on_delete=models.CASCADE)
+
 class Order(models.Model):
+
     desk_no = models.CharField(max_length=4)
     name_of_cuisine = models.CharField(max_length=30)
     status = models.CharField(max_length=10)
     time = models.DateField(default = timezone.now().strftime("%d/%m/%y"))
     amount = models.IntegerField()
-    # to store: many to one
     store = models.ForeignKey(Store,on_delete=models.CASCADE)
-
-class Store(models.Model):
-    # to manager: many to many
-    manager = models.ManyToManyField(Manager,on_delete=models.CASCADE)
-    location = models.CharField(max_length=100)
-    name = models.CharField(max_length=10)
 
 class Employee(models.Model):
     name = models.CharField(max_length=20)
-    # to store: many to many
-    store = models.ManyToManyField(Manager)
-    
+    e_store = models.ForeignKey(Store,on_delete=models.CASCADE)
     manager = models.ForeignKey(Manager,on_delete=models.CASCADE)
 
-class Manager(models.Model):
-    name = models.CharField(max_length=20)
 
 
 
