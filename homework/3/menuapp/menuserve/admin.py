0f0951659fc746a063c1102b62cdd847568f099e
nicholeemma <790956043@qq.com>
2019-09-16 13:38:34 -0400

    Register model in admin

--- homework/3/menuapp/menuserve/admin.py
+++ homework/3/menuapp/menuserve/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ("location",  "name")
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ("name",)
+# admin.site.register(models.TodoList, TodoListAdmin)
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("name_of_cuisine",  "id_for_dish","price","classification","description")
+
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = ("name",  "m_store")
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("desk_no",  "name_of_cuisine","status","time","amount","store")
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("name",  "e_store","manager")
+
+admin.site.register(models.Store, StoreAdmin)
+admin.site.register(models.Menu, MenuAdmin)
+admin.site.register(models.Manager, ManagerAdmin)
+admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.Employee, EmployeeAdmin)
