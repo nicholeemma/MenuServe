815aa977942b3400c435a8320e198b736c297235
nicholeemma <790956043@qq.com>
2019-10-02 00:09:19 -0400

    copy files from branch hw4_deployment

--- homework/4/menuapp/urls.py
+++ homework/4/menuapp/urls.py
@@ -19,7 +19,7 @@ from menuserve.views import index
 from menuserve.views import home
 from menuserve.views import manageorders
 from menuserve.views import (managermain,manageremployee,managerstore,managermenu,managermanager)
-from django.conf.urls import url
+from django.conf.urls import include,url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
@@ -29,19 +29,24 @@ from django.views.static import serve
 
 
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('',index,name="index"),
+#     # url(r'^Menu/', index),
+#     url(r'^Order/',home,name="Order"),
+#     url(r'^Submitted-Order/',manageorders,name="manageorder"),
+#     url(r'^Manager-Main/',managermain,name="managermain"),
+#     url(r'^Manager-Employee/',manageremployee,name="manageremployee"),
+#     url(r'^Manager-Store/',managerstore,name="managerstore"),
+#     url(r'^Manager-Menu/',managermenu,name="managermenu"),
+#     url(r'^Manager-Manager/',managermanager,name="managermanager"),
+#    #s url(r'^media/(?P<path>.*)$', serve, {"document_root":settings.MEDIA_URL}),
+# ]
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',index,name="index"),
-    # url(r'^Menu/', index),
-    url(r'^Order/',home,name="Order"),
-    url(r'^Submitted-Order/',manageorders,name="manageorder"),
-    url(r'^Manager-Main/',managermain,name="managermain"),
-    url(r'^Manager-Employee/',manageremployee,name="manageremployee"),
-    url(r'^Manager-Store/',managerstore,name="managerstore"),
-    url(r'^Manager-Menu/',managermenu,name="managermenu"),
-    url(r'^Manager-Manager/',managermanager,name="managermanager"),
-   #s url(r'^media/(?P<path>.*)$', serve, {"document_root":settings.MEDIA_URL}),
-]
+path('admin/', admin.site.urls),
+     path('',index,name="index"),
+     path('',include('menuserve.urls'))]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- homework/4/menuserve/views.py
+++ homework/4/menuserve/views.py
@@ -7,7 +7,35 @@ def index(request):
     Function for main page
     '''
     
+    content={}
+    error_message=""
     menus = Menu.objects.all()
+    if request.method == "POST":
+        if "search" in request.POST:
+            try:
+                name = request.POST["searchcontent"]
+                if name=="":
+                    menus = Menu.objects.all()
+                else:
+                    menus = Menu.objects.filter(name_of_cuisine=name)
+            except:
+                content["show"]="What you deleted does not exist"
+                error_message = content["show"]
+                return render(request,"Menu.html",{"menus":menus,"show":error_message})
+        if "select" in request.POST:
+            menus = ""
+            try:
+                # getlist method is important to remember
+                checkedlist = request.POST.getlist('checkedbox')
+                #checkedlist = request.POST["checkedbox"] 
+                for checked_classification in checkedlist:
+                    
+                    menus = Menu.objects.filter(classification=checked_classification)
+                   
+            except:
+                content["show"]="What you deleted does not exist"
+                error_message = content["show"]
+                return render(request,"Menu.html",{"menus":menus,"show":error_message})
     
     
     return render(request,"Menu.html",{"menus":menus})
@@ -65,20 +93,15 @@ def home(request):
                 return render(request,"Order.html",{"orders":orders,"stores":stores,"menus":menus,"show":error_message}) 
             time = "2019-09-21"
             
-           # 
-            an_order = Order(desk_no=desk_, name_of_cuisine=name_of_cuisine, time=time,
-                                status=status, amount=amount, store=store ,price=price)
-            an_order.save() #saving 
-            return redirect("/Order/")
-
-            
-    
-        # content["show"]="Error! check your input"
-        # error_message = content["show"]
-        # return render(request,"Order.html",{"orders":orders,"stores":stores,"menus":menus,"show":error_message}) 
-
-
-    
+            try:
+                an_order = Order(desk_no=desk_, name_of_cuisine=name_of_cuisine, time=time,
+                                    status=status, amount=amount, store=store ,price=price)
+                an_order.save() #saving 
+            except:
+                content["show"]="Input does not comply with rules. Check your input"
+                error_message = content["show"]
+                return render(request,"Order.html",{"orders":orders,"stores":stores,"menus":menus,"show":error_message}) 
+            return redirect("/Order/")    
     return render(request,"Order.html",{"stores":stores,"orders":orders,"menus":menus,"show":error_message})
 
 def manageorders(request):
@@ -108,8 +131,14 @@ def manageorders(request):
                 content["show"]="order does not exist"
                 error_message = content["show"]
                 return render(request,"Submitted-Order.html",{"orders":orders,"stores":stores,"menus":menus,"show":error_message})
-            u_order.desk_no = str(u_input_desk_no)
-            u_order.name_of_cuisine = str(u_cuisine_select)
+            try:    
+                u_order.desk_no = str(u_input_desk_no)
+                u_order.name_of_cuisine = str(u_cuisine_select)
+                u_order.status = u_input_status
+            except:
+                content["show"]="Input exceeds the allowed length"
+                error_message = content["show"]
+                return render(request,"Submitted-Order.html",{"orders":orders,"stores":stores,"menus":menus,"show":error_message})
             try:
                 u_order.amount = int(u_input_amount)
             except:
@@ -121,8 +150,7 @@ def manageorders(request):
             except:
                 content["show"]="menu does not exist"
                 error_message = content["show"]
-                return render(request,"Submitted-Order.html",{"orders":orders,"stores":stores,"menus":menus,"show":error_message})
-            u_order.status = u_input_status
+                return render(request,"Submitted-Order.html",{"orders":orders,"stores":stores,"menus":menus,"show":error_message})               
             #u_order.time = u_input_time
             try:
                 u_order.store = Store.objects.get(name = str(u_input_store ))
@@ -130,8 +158,14 @@ def manageorders(request):
                 content["show"]="Store does not exist"
                 error_message = content["show"]
                 return render(request,"Submitted-Order.html",{"orders":orders,"stores":stores,"menus":menus,"show":error_message}) 
+            try:
 
-            u_order.save()
+                u_order.save()
+            except:
+                content["show"]="input does not comply with rules, check input"
+                error_message = content["show"]
+                return render(request,"Submitted-Order.html",{"orders":orders,"stores":stores,"menus":menus,"show":error_message}) 
+            
             return redirect("/Submitted-Order/")
         if "DeleteUpdate" in request.POST: #checking if there is a request to delete a todo
             d_order = request.POST["DeleteUpdate"] #checked todos to be deleted
@@ -164,9 +198,9 @@ def managerstore(request):
     stores = Store.objects.all()
     managers = Manager.objects.all()
     if request.method == "POST": #checking if the request method is a POST
-        if "StoreAdd" in request.POST: #checking if there is a request to add a todo
-            location = request.POST["location"] #title
-            name = request.POST["name"] #date
+        if "StoreAdd" in request.POST: 
+            location = request.POST["location"] 
+            name = request.POST["name"] 
             manager_name = request.POST["manager_select"]
             try:
                 manager = Manager.objects.get(name=str(manager_name))
@@ -174,9 +208,13 @@ def managerstore(request):
                 content["show"]="Manager does not exist"
                 error_message = content["show"]
                 return render(request,"Manager-Store.html",{"stores":stores,"managers":managers,"show":error_message}) 
-            #content = title + " -- " + date + " " + category #content
-            a_store = Store(name=name, location=location,store_manager=manager)
-            a_store.save() #saving the todo 
+            try:
+                a_store = Store(name=name, location=location,store_manager=manager)
+                a_store.save() 
+            except:
+                content["show"]="Add cannot be done, check your input"
+                error_message = content["show"]
+                return render(request,"Manager-Store.html",{"stores":stores,"managers":managers,"show":error_message}) 
             return redirect("/Manager-Store/")
         if "StoreDelete" in request.POST: #checking if there is a request to delete a todo
             d_store_id = request.POST["StoreDelete"] #checked todos to be deleted
@@ -189,23 +227,31 @@ def managerstore(request):
                 return render(request,"Manager-Store.html",{"stores":stores,"managers":managers,"show":error_message}) 
             d_store.delete()
             return redirect("/Manager-Store/")
-        if "StoreUpdate" in request.POST: #checking if there is a request to delete a todo
-            u_store_id = request.POST["StoreUpdate"]
-            u_store_location = request.POST["input_storelocation"] #checked todos to be deleted
-            # for todo_id in checkedlist:
-            u_store_name = request.POST["input_storename"]
-            u_store_manager = request.POST["manager_select"]
-            u_store = Store.objects.get(id=u_store_id)
-            u_store.location = str(u_store_location)
-            u_store.name = str(u_store_name)
+        if "StoreUpdate" in request.POST: 
+            try:
+                u_store_id = request.POST["StoreUpdate"]
+                u_store_location = request.POST["input_storelocation"]            
+                u_store_name = request.POST["input_storename"]
+                u_store_manager = request.POST["manager_select"]
+                u_store = Store.objects.get(id=u_store_id)
+                u_store.location = str(u_store_location)
+                u_store.name = str(u_store_name)
+            except:
+                content["show"]="update cannot be done, check your input"
+                error_message = content["show"]
+                return render(request,"Manager-Store.html",{"stores":stores,"managers":managers,"show":error_message}) 
             try:
                 u_store.store_manager  = Manager.objects.get(name=str(u_store_manager))
             except:
                 content["show"]="Manager does not exist"
                 error_message = content["show"]
                 return render(request,"Manager-Store.html",{"stores":stores,"managers":managers,"show":error_message}) 
-            #getting todo id
-            u_store.save()
+            try:
+                u_store.save()
+            except:
+                content["show"]="update cannot be done, check your input"
+                error_message = content["show"]
+                return render(request,"Manager-Store.html",{"stores":stores,"managers":managers,"show":error_message})
             return redirect("/Manager-Store/")
 
     return render(request,"Manager-Store.html",{"stores":stores,"managers":managers})
@@ -219,14 +265,17 @@ def managermanager(request):
 
     
     managers = Manager.objects.all()
-    if request.method == "POST": #checking if the request method is a POST
-        if "ManagerAdd" in request.POST: #checking if there is a request to add a todo
-            gender = request.POST["gender"] #title
-            name = request.POST["name"] #date
-            
-            #content = title + " -- " + date + " " + category #content
-            a_manager = Manager(name=name, gender=gender)
-            a_manager.save() #saving the todo 
+    if request.method == "POST": 
+        if "ManagerAdd" in request.POST: 
+            gender = request.POST["gender"] 
+            name = request.POST["name"] 
+            try:
+                a_manager = Manager(name=name, gender=gender)
+                a_manager.save() 
+            except:
+                content["show"]="Add cannot be done, check your input"
+                error_message = content["show"]
+                return render(request,"Manager-Manager.html",{"managers":managers,"show":error_message}) 
             return redirect("/Manager-Manager/")
         if "ManagerDelete" in request.POST: #checking if there is a request to delete a todo
             d_manager_id = request.POST["ManagerDelete"] #checked todos to be deleted
@@ -251,7 +300,12 @@ def managermanager(request):
                 return render(request,"Manager-Manager.html",{"managers":managers,"show":error_message}) 
             u_manager.location = str(u_manager_gender)
             u_manager.name = str(u_manager_name)  #getting todo id
-            u_manager.save()
+            try:
+                u_manager.save()
+            except:
+                content["show"]="Check your input"
+                error_message = content["show"]
+                return render(request,"Manager-Manager.html",{"managers":managers,"show":error_message})
             return redirect("/Manager-Manager/")
 
     
@@ -273,9 +327,29 @@ def manageremployee(request):
     managers = Manager.objects.all()
     employees = Employee.objects.all()
     if request.method == "POST": #checking if the request method is a POST
-        if "EmployeeAdd" in request.POST: #checking if there is a request to add a todo
-                #title
-            name = request.POST["name"] #date
+        if "search" in request.POST:
+            try:
+                name = request.POST["searchcontent"]
+                if name=="":
+                    employees = Employee.objects.all()
+                else:
+                    employees = Employee.objects.filter(name=name)
+            except:
+                content["show_error"]="Employee does not exist?"
+                error_message = content["show_error"]
+                return render(request,"Manager-Employee.html",{"stores":stores,"managers":managers,"show_error":error_message,"employees":employees}) 
+   
+# This select function will be realized in the future   
+        # if "store-selct" in request.POST:
+        #     selected_store = request.POST["selectedstore"]
+        #     #employees = Employee.e_store_set.filter(e_store__contains=selected_store).distinct()
+        #     # a = Store.objects.get(id=selected_store )
+        #     # employees = a.employee_set.all()
+        #     e = Store.objects.get(id=selected_store)
+        #     employees = e.employees.all()
+        if "EmployeeAdd" in request.POST:
+               
+            name = request.POST["name"] 
             manager_name = request.POST["manager_select"]
             try:
                 manager = Manager.objects.get(name=str(manager_name))
@@ -292,9 +366,14 @@ def manageremployee(request):
                 content["show_error"]="Store does not exist"
                 error_message = content["show_error"]
                 return render(request,"Manager-Employee.html",{"stores":stores,"managers":managers,"show_error":error_message,"employees":employees}) 
-            a_employee = Employee.objects.create(name=name,manager=manager)
-            a_employee.e_store.add(store)
-            a_employee.save() #saving the todo 
+            try:
+                a_employee = Employee.objects.create(name=name,manager=manager)
+                a_employee.e_store.add(store)
+                a_employee.save() 
+            except:
+                content["show_error"]="Add cannot be done, check your input"
+                error_message = content["show_error"]
+                return render(request,"Manager-Employee.html",{"stores":stores,"managers":managers,"show_error":error_message,"employees":employees}) 
             for s in a_employee.e_store.all():
                 store_message+=str(s.name)
             
@@ -324,19 +403,20 @@ def manageremployee(request):
                 return render(request,"Manager-Employee.html",{"stores":stores,"managers":managers,"show_error":error_message,"employees":employees})  #getting todo id
             
             
-            u_employee.name = str(u_employee_name)
+            
             try:
                 u_employee.manager  = Manager.objects.get(name=str(u_employee_manager))
             except:
                 content["show_error"]="manager does not exist"
                 error_message = content["show_error"]
                 return render(request,"Manager-Employee.html",{"stores":stores,"managers":managers,"show_error":error_message,"employees":employees})  #getting todo id
+            try:
+                u_employee.save()
+            except:
+                content["show_error"]="Employee's name is too long"
+                error_message = content["show_error"]
+                return render(request,"Manager-Employee.html",{"stores":stores,"managers":managers,"show_error":error_message,"employees":employees})  #getting todo id
             
-           
-            
-            #getting todo id
-            #getting todo id
-            u_employee.save()
             return redirect("/Manager-Employee/")
         if "EmployeeAddStore" in request.POST:
             u_employee_store = request.POST["store_select"]
@@ -411,8 +491,13 @@ def managermenu(request):
                 return render(request,"Manager-Menu.html",{"menus":menus,"show":error_message})
             category = request.POST['menu_select']
             description = request.POST['description']
-            a_menu = Menu(picture=uploaded_file_url, id_for_dish=id_for_dish, name_of_cuisine=name_of_cuisine,price=price,classification=category,description=description)
-            a_menu.save() #saving the todo 
+            try:
+                a_menu = Menu(picture=uploaded_file_url, id_for_dish=id_for_dish, name_of_cuisine=name_of_cuisine,price=price,classification=category,description=description)
+                a_menu.save() 
+            except:
+                content["show"]="the input should comply with rules, check your input"
+                error_message = content["show"]
+                return render(request,"Manager-Menu.html",{"menus":menus,"show":error_message}) 
             return redirect("/Manager-Menu/")
         if "MenuDelete" in request.POST: #checking if there is a request to delete a todo
             d_Menu_id = request.POST["MenuDelete"]
@@ -425,17 +510,21 @@ def managermenu(request):
             d_menu.delete()
             return redirect("/Manager-Menu/")
         if "MenuUpdate" in request.POST: #checking if there is a request to update
-            myfile = request.FILES['myfileupdate']
-            fs = FileSystemStorage()
-            filename = fs.save(myfile.name, myfile)
-            uploaded_file_url = fs.url(filename)
+            try:
+                myfile = request.FILES['myfileupdate']
+                fs = FileSystemStorage()
+                filename = fs.save(myfile.name, myfile)
+                uploaded_file_url = fs.url(filename)
 
-            u_Menu_id = request.POST["MenuUpdate"]
-            u_Menu_id_dish = request.POST["input_menuid_for_dish"] 
-            u_menu_name = request.POST["input_menuname_of_cuisine"]
-            u_category = request.POST['menu_select']
-            u_description = request.POST['input_menudescription']
-            
+                u_Menu_id = request.POST["MenuUpdate"]
+                u_Menu_id_dish = request.POST["input_menuid_for_dish"] 
+                u_menu_name = request.POST["input_menuname_of_cuisine"]
+                u_category = request.POST['menu_select']
+                u_description = request.POST['input_menudescription']
+            except:
+                content["show"]="the input should comply with rules, check your input"
+                error_message = content["show"]
+                return render(request,"Manager-Menu.html",{"menus":menus,"show":error_message}) 
             try:
                 u_price = int(request.POST['input_menuprice'])
             except:
@@ -454,7 +543,13 @@ def managermenu(request):
             u_menu.description = str(u_description) 
             u_menu.price = str(u_price)  
             u_menu.picture = uploaded_file_url
-            u_menu.save()
+            try:
+
+                u_menu.save()
+            except:
+                content["show"]="the input should comply with rules, check your input"
+                error_message = content["show"]
+                return render(request,"Manager-Menu.html",{"menus":menus,"show":error_message}) 
             return redirect("/Manager-Menu/")
 
     
