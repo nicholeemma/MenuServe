dbf139b0a299623197342bec3c81bb8e26cb2c9e
jiayuespiraldata <jiayue@spiraldatagroup.com.au>
2020-04-30 16:32:59 +0930

    Add the function of hidding menu pics

---
+++ menuserve/static/js/hide.js
@@ -0,0 +1,11 @@
+
+function hide() {
+    if (document.getElementById("container-pic1").style.display =="") {
+        document.getElementById("container-pic1").style.display="none"
+    } else {
+        document.getElementById("container-pic1").style.display="";
+    }
+
+
+
+}
